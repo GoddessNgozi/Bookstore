c10f42c049e6fb01e96c317ed455dab1cc6b010b
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-04 06:39:36 +0100

    Update components

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,20 +2,31 @@ import React from 'react';
 
 const Book = ({ title, author }) => {
     return (
-      <div>
+      <div className='book-row'>
         <div>
-          <h3>{title}</h3>
+          <h2>{title}</h2>
           <p>{author}</p>
-          <div>
-            <span>Comments</span>
-            <span>Remove</span>
-            <span>Edit</span>
-          </div>
-        </div>
-  
         <div>
-          <span>Current Chapter</span>
-          <span>Chapter 17</span>
+            <button>Comments</button>
+            <button>Remove</button>
+            <button>Edit</button>
+        </div>
+      </div>
+
+        <div className='progressor'>
+        <div className="circular-progress-container">
+          <div className="circular-progress"></div>
+        </div>
+        <div class="progress-stat">
+          <p class="percent-complete">64%</p>
+          <p class="completed">Completed</p>
+        </div>
+        </div>
+
+        <div>
+          <p>Current Chapter</p>
+          <p>Chapter 17</p>
+          <button>UPDATE PROGRESS</button>
         </div>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <h1>Bookstore CMS</h1>
-      <nav>
-        <ul>
+      <div>
+        <div className='left'>
+          <h1>Bookstore CMS</h1>
+          <nav>
+          <ul className='links'>
           <li>
             <Link to="/">Books</Link>
           </li>
@@ -14,7 +16,13 @@ const Header = () => {
             <Link to="/categories">Categories</Link>
           </li>
         </ul>
-      </nav>
+          </nav>
+        </div>
+      </div>
+
+      <div>
+        <span className="material-symbols-rounded">person</span>
+      </div>
     </header>
   );
 }
