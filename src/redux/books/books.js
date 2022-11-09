ffcb0cf2e6a60ae7e6b1e070abd454416316f643
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-09 11:01:16 +0100

    Change empty default state to an array with books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,8 +2,26 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
+const defaultState = [
+  {
+    id: 1,
+    title: 'Book title',
+    author: 'Author',
+  },
+  {
+    id: 2,
+    title: 'Book title',
+    author: 'Author',
+  },
+  {
+    id: 3,
+    title: 'Book title',
+    author: 'Author',
+  },
+];
+
 // Reducer
-const booksReducer = (state = [], action) => {
+const booksReducer = (state = defaultState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload.book];
