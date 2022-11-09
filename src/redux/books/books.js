0feaa234634f36066c492c4c73cf25198ceb6b94
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-09 09:31:00 +0100

    Refactor reducers and actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,34 +1,28 @@
 // Actions
-const ADD_BOOK = 'redux/books/ADD_BOOK';
-const REMOVE_BOOK = 'redux/books/REMOVE_BOOK';
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 // Reducer
 const booksReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false,
-        },
-      ];
+      return [...state, action.payload.book];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.id);
-
+      return state.filter((book) => book.id !== action.payload.id);
     default:
       return state;
   }
 };
 
 // Action Creators
-const addBook = () => ({
+const addBook = (book) => ({
   type: ADD_BOOK,
+  payload: book,
 });
 
-const removeBook = () => ({
+const removeBook = (id) => ({
   type: REMOVE_BOOK,
+  payload: id,
 });
 
 export default booksReducer;
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,12 +1,11 @@
-// Actions
-const CHECK_STATUS = 'redux/categories/CHECK_STATUS';
+// Action
+const CHECK_STATUS = 'bookstore/books/CHECK_STATUS';
 
 // Reducer
 const categoriesReducer = (state = [], action) => {
   switch (action.type) {
     case CHECK_STATUS:
       return 'Under construction';
-
     default:
       return state;
   }
