0cb703584338f1eb8d3bda2e8b990cece254856f
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-11-10 21:41:19 +0100

    Fetch books from bookstore API on pageload

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBooks } from '../redux/books/books';
 import Book from './Book';
 import Form from './Form';
 
 const Books = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [books, dispatch]);
+
   return (
-    <>
+    <div>
       {books.map((book) => (
         <Book key={book.id} id={book.id} title={book.title} author={book.author} />
       ))}
       <Form />
-    </>
+    </div>
   );
 };
 
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,30 +1,17 @@
-import { v4 as uuidv4 } from 'uuid';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 // Actions
+const GET_BOOKS = 'bookstore/books/GET_BOOKS';
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const defaultState = [
-  {
-    id: uuidv4(),
-    title: 'Book title',
-    author: 'Author',
-  },
-  {
-    id: uuidv4(),
-    title: 'Book title',
-    author: 'Author',
-  },
-  {
-    id: uuidv4(),
-    title: 'Book title',
-    author: 'Author',
-  },
-];
+const defaultState = [];
 
 // Reducer
 const booksReducer = (state = defaultState, action) => {
   switch (action.type) {
+    case GET_BOOKS:
+      return action.payload;
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
@@ -34,7 +21,32 @@ const booksReducer = (state = defaultState, action) => {
   }
 };
 
+const toArray = (data) => {
+  const bookArray = Object.entries(data).map(([key, val]) => {
+    const { title, author, category } = val[0];
+    return {
+      id: key,
+      title,
+      author,
+      category,
+    };
+  });
+  return bookArray;
+};
+
 // Action Creators
+export const getBooks = createAsyncThunk(GET_BOOKS, async (post, { dispatch }) => {
+  const response = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/QWE80YAxuTk35K5Gx26Z/books');
+  const data = await response.json();
+  const books = toArray(data);
+  if (books) {
+    dispatch({
+      type: GET_BOOKS,
+      payload: books,
+    });
+  }
+});
+
 const addBook = (book) => ({
   type: ADD_BOOK,
   payload: book,
